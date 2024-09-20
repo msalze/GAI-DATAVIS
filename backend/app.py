4cf2289bd40ff34cf1a4af30f59b3fbe7b68226a
uensal <uensal4@gmail.com>
2024-09-20 11:32:58 +0200

    minimal change in app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ import os
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
 @app.after_request
 def after_request(response):
